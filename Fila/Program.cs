a9898aaec7f9a22afd37ab5f75d57cf41192ceaf
Erbson Lavras <erbson.org@gmail.com>
2020-11-17 14:34:21 -0300

    Adicionar arquivos de projeto.

---
+++ Fila/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<String> Fila = new Queue<String>();
+            //Enfileirando
+
+            Fila.Enqueue("A");
+            Fila.Enqueue("B");
+            Fila.Enqueue("C");
+            Fila.Enqueue("D");
+            Fila.Enqueue("E");
+
+            //Removendo da Fila
+            Fila.Dequeue();
+
+            foreach (String x in Fila)
+            {
+                Console.WriteLine(x);
+
+            }
+            Console.WriteLine("Mostrando o primeiro da Fila");
+            Console.WriteLine(Fila.Peek());
+            Console.ReadKey();
+
+           
+        }
+    }
+}
